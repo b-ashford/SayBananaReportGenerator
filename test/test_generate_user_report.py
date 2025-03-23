1ad74d1a8e8ec98cfccae9b32b2ab1246a77b856
Brono25 <bronston.a@icloud.com>
2025-03-23 20:09:26 +1100

    removed test

--- test/test_generate_user_report.py
+++
@@ -1,9 +0,0 @@
-
-from SBReportGenerator import generate_user_report
-
-
-
-files = ["test/user_productions_example.txt"]
-outputs = ["output/test1.pdf", "output/test2.pdf"]
-for file, output in zip(files, outputs):
-    generate_user_report(file, output)
