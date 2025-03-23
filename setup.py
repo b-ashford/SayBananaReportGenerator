a221b9d36801373a803d6022bba8f047b1b0e999
Brono25 <bronston.a@icloud.com>
2025-03-23 19:51:46 +1100

    include test file

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup, find_packages
 
-
 with open("requirements.txt", mode="r", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 setup(
     name="SBReportGenerator",
     version="0.1.0",
-    packages=find_packages(where="src"),
+    packages=find_packages(
+        where="src"
+    ), 
     package_dir={"": "src"},
     install_requires=requirements,
     exclude=("__pycache__",),
     description="SayBanana app user productions report generator.",
     author="Bronston Ashford",
-    package_data={
-        "": ["images/*.png"],
-    },
     include_package_data=True,
+    package_data={
+        "SBReportGenerator": ["images/*.png", "data/user_productions_example.txt"],
+    },
 )
